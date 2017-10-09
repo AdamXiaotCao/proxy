6d6da192df8a1fbdf0587ce581fe742b48c0bcd2
AdamXiaotCao <xiaotcao@gmail.com>
2017-10-08 22:02:08 -0700

    Listen on Port:PORT

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+if (module === require.main) {
+  // [START server]
+    // Start the server
+  const server = app.listen(process.env.PORT || 8081, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+    // [END server]
+}
+module.exports = app;
