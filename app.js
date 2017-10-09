529a1caec1939d9bc4edb7567c95069c789490f7
AdamXiaotCao <xiaotcao@gmail.com>
2017-10-08 22:42:47 -0700

    clean up

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ app.get('/', function (req, res) {
   res.send('My Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
 if (module === require.main) {
   // [START server]
     // Start the server
